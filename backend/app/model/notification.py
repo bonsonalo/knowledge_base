96f297d2237f48d6f6df35edc033d0175248c889
bonsonalo <bonson2468@gmail.com>
2026-03-18 13:11:51 +0800

    feat: add notification model

---
+++ backend/app/model/notification.py
@@ -0,0 +1,37 @@
+from sqlalchemy import String, UUID, ForeignKey, Boolean
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+import uuid
+from app.core.database import Base
+from datetime import datetime, timezone
+
+
+class Notification(Base):
+    __tablename__= "notification"
+
+
+    id: Mapped[UUID]= mapped_column(
+        UUID(as_uuid= True),
+        primary_key= True,
+        default= uuid.uuid4
+    )
+    user_id: Mapped[UUID]= mapped_column(
+        ForeignKey("users.id", ondelete= "CASCADE")
+    )
+    message: Mapped[str]= mapped_column(
+        String
+    )
+    is_read: Mapped[bool]= mapped_column(
+        Boolean,
+        default= False
+    )
+    created_at: Mapped[datetime]= mapped_column(
+        datetime,
+        default= datetime.now(timezone.utc)
+    )
+
+
+    user: Mapped["User"]= relationship(
+        "User",
+        back_populates= "user",
+        lazy= "selectin"
+    )
