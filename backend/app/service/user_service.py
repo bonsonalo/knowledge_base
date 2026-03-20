4bba1d453d4cb01a2c11ec2b5fc6e4000633a736
bonsonalo <bonson2468@gmail.com>
2026-03-20 17:49:13 +0800

    feat: add user_service for get and update profile

---
+++ backend/app/service/user_service.py
@@ -0,0 +1,52 @@
+from sqlalchemy import select
+from app.model.user import User
+from app.core.logger import logger
+from app.schema.user_schema import ProfileUpdate
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+
+
+# get user profile
+async def user_profile_service(current_user, db: AsyncSession):
+        current_id= current_user["id"]
+        user= await db.scalar(select(User).where(User.id == current_id))
+        if not user:
+            logger.error("User doesnt exist")
+            raise ValueError("user not found")
+        return user
+
+
+#update profile
+
+async def update_profile_service(to_update: ProfileUpdate, current_user, db: AsyncSession):
+    current_id= current_user["id"]
+    user= await db.scalar(select(User).where(User.id == current_id))
+
+    if not user:
+         raise ValueError("user not available")
+    if to_update.first_name is not None:
+         user.first_name = to_update.first_name
+    if to_update.last_name is not None:
+         user.last_name = to_update.last_name
+    if to_update.avatar is not None:
+         user.avatar = to_update.avatar
+
+    db.commit()
+    db.refresh(user)
+    return user
+
+# upload profile picture
+
+##########
+##########
+
+
+
+# change password
+
+
+#####
+      
+
+
