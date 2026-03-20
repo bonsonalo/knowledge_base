a435328242791d1e04b13d447cec0f6efe06d8f9
bonsonalo <bonson2468@gmail.com>
2026-03-20 08:00:45 +0800

    faet: add auth schema

---
+++ backend/app/schema/auth_schema.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, EmailStr
+from enum import Enum
+
+
+class UserSignUp(BaseModel):
+    first_name: str
+    last_name: str
+    email: EmailStr
+    password: str
+
+
+class LoginInfo(BaseModel):
+    email: EmailStr
+    password: str
+
+class Role(str, Enum):
+    user= "user",
+    editor= "editor",
+    admin = "admin"
+
+class RefreshTokenRequest(BaseModel):
+    refresh_token: str
