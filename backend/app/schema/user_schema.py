356aaf75b7a983d555ddd3b20739f584e6bd8db3
bonsonalo <bonson2468@gmail.com>
2026-03-21 23:07:59 +0800

    feat: set up user schema

---
+++ backend/app/schema/user_schema.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+
+
+class UserProfile(BaseModel):
+    first_name: str
+    last_name: str
+    email: str
+    avatar: str | None
+
+class ProfileUpdate(BaseModel):
+    first_name: str | None
+    last_name: str | None
+    avatar: str | None
