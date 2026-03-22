e3556595d9b7de55fca42d6efd561e57e349fbeb
bonsonalo <bonson2468@gmail.com>
2026-03-22 23:09:54 +0800

    feat: add router from auth endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-
+from app.api.v1.routes import routers
 
 
 @asynccontextmanager
@@ -14,3 +14,7 @@ app = FastAPI(title= "Knowledge Base", lifespan= lifespan)
 @app.get("/")
 async def get_response():
     return "Hey"
+
+
+
+app.include_router(routers)
