d4ab49f9ff273c67804c604445119e4d8850f578
bonsonalo <bonson2468@gmail.com>
2026-03-23 13:21:57 +0800

    feat: add articlecschema

---
+++ backend/app/schema/article_schema.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel
+from enum import Enum
+
+
+
+
+class Category(str, Enum):
+    technology = "technology"
+    science = "science"
+    health = "health"
+    business = "business"
+    education = "education"
+    finance = "finance"
+    programming = "programming"
+    design = "design"
+    marketing = "marketing"
+    productivity = "productivity"
+    artificial_intelligence = "artificial_intelligence"
+    cybersecurity = "cybersecurity"
+    personal_development = "personal_development"
+    research = "research"
+    other = "other"
+
+
+class CreateArticle(BaseModel):
+    title: str
+    content: str
+    cover_image: str
+    category: Category
+    
+
+class ToUpdate(BaseModel):
+    title: str | None
+    content: str | None
+    cover_image: str | None
+    category: Category | None
+
+
