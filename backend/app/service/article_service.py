5a0143f3800979305b11e5faec49ce12a47afe1f
bonsonalo <bonson2468@gmail.com>
2026-03-25 19:15:27 +0800

    feat: add business logic for admin role and users that have not logged in

--- backend/app/service/article_service.py
+++ backend/app/service/article_service.py
@@ -7,7 +7,7 @@ from uuid import UUID
 
 
 
-# create and publish article
+# create and publish article    Editor Role
 
 async def create_article_publish(to_add: CreateArticle, current_user, db: AsyncSession):
     current_id= current_user.get("id")
@@ -35,7 +35,7 @@ async def create_article_publish(to_add: CreateArticle, current_user, db: AsyncS
         raise ValueError(str(e))
     
 
-# create and draft artcile
+# create and draft artcile           Editor Role
 
 async def create_article_draft(to_add: CreateArticle, current_user, db: AsyncSession):
     current_id= current_user.get("id")
@@ -63,7 +63,7 @@ async def create_article_draft(to_add: CreateArticle, current_user, db: AsyncSes
         raise ValueError(str(e))
     
 
-# update article
+# update article    Editor Role
 
 async def patch_article(to_update: ToUpdate, current_user, db: AsyncSession):
     current_id= current_user.get("id")
@@ -83,7 +83,7 @@ async def patch_article(to_update: ToUpdate, current_user, db: AsyncSession):
     await db.refresh(article)
     return article
 
-# get all articles that you published or drafted
+# get all articles that YOU published or drafted       Editor Role
 
 async def get_all_articles(current_user, db: AsyncSession):
     current_id= current_user["id"]
@@ -95,7 +95,56 @@ async def get_all_articles(current_user, db: AsyncSession):
         raise ValueError(str(e))
     
 
-#get single article
+# continues from the above. it is for Published
+
+async def get_all_articles(current_user, db: AsyncSession):
+    current_id= current_user["id"]
+    try:
+        articles= await db.execute(select(Article).where(Article.author_id == current_id).where(Article.status == "published"))
+        return articles.scalars().all()
+    except ValueError as e:
+        logger.error(str(e))
+        raise ValueError(str(e))
+    
+
+# continues from the above. it is for draft
+
+async def get_all_articles(current_user, db: AsyncSession):
+    current_id= current_user["id"]
+    try:
+        articles= await db.execute(select(Article).where(Article.author_id == current_id).where(Article.status == "draft"))
+        return articles.scalars().all()
+    except ValueError as e:
+        logger.error(str(e))
+        raise ValueError(str(e))
+
+
+# get all articles     # no need to login
+
+async def get_all_articles( db: AsyncSession):
+    try:
+        articles= await db.execute(select(Article).where(Article.status == "published"))
+        return articles.scalars().all()
+    except ValueError as e:
+        logger.error(str(e))
+        raise ValueError(str(e))
+
+#get single article     # no need to log in
+
+async def get_article(article_id: UUID, db: AsyncSession):
+    try:
+        article= await db.scalar(select(Article).where((Article.id == article_id)).where(Article.status == "published"))
+        return article
+    except ValueError as e:
+        logger.error(str(e))
+        raise ValueError(str(e))
+
+
+
+
+
+
+#get single article     # EDITOR ROLE
 
 async def get_article(article_id: UUID, current_user, db: AsyncSession):
     current_id= current_user["id"]
@@ -107,7 +156,7 @@ async def get_article(article_id: UUID, current_user, db: AsyncSession):
         raise ValueError(str(e))
     
 
-# delete article
+# delete article Editor Role
 
 async def delete_article(article_id: UUID, current_user, db: AsyncSession):
     current_id= current_user["id"]
@@ -119,4 +168,18 @@ async def delete_article(article_id: UUID, current_user, db: AsyncSession):
     except ValueError as e:
         logger.error(str(e))
         raise ValueError(str(e))
+    
+
+# delete article for admin role
+
+async def delete_article(article_id: UUID, db: AsyncSession):
+    try:
+        article= await db.scalar(select(Article).where((Article.id == article_id)))
+        await db.delete(article)
+        await db.commit()
+        return {"message": "Article deleted successfully"}
+    except ValueError as e:
+        logger.error(str(e))
+        raise ValueError(str(e))
+
 
