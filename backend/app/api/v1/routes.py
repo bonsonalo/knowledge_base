c511f119c42a2eef05566396f726d4ad248cefe7
bonsonalo <bonson2468@gmail.com>
2026-03-26 04:37:44 +0800

    feat: add router for article endpoint

--- backend/app/api/v1/routes.py
+++ backend/app/api/v1/routes.py
@@ -1,12 +1,13 @@
 from fastapi import APIRouter
 from app.api.v1.endpoints.auth import router as auth_router
+from app.api.v1.endpoints.article import router as article_router
 
 
 
 
 routers= APIRouter()
 
-router_list= [auth_router]
+router_list= [auth_router, article_router]
 
 for router in router_list:
     routers.include_router(router)
