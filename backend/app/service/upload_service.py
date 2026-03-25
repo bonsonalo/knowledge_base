78d2ddaeeb8ac8d9c25898dbef95c05f8c6e491d
bonsonalo <bonson2468@gmail.com>
2026-03-26 02:00:00 +0800

    feat: add upload_file function to upload it to the cloudinary

---
+++ backend/app/service/upload_service.py
@@ -0,0 +1,24 @@
+
+from fastapi import UploadFile
+import cloudinary.uploader
+
+
+
+
+async def upload_file(file: UploadFile, folder: str):
+    allowed_types= ["image/jpeg", "image/png", "image/webp"]
+    if file.content_type not in allowed_types:
+        raise ValueError("Invalid file type")
+    # validate size (2MB)
+    contents = await file.read()
+    if len(contents) > 2 * 1024 * 1024:
+        raise ValueError("File too large")
+    
+    # upload to cloudinary
+    result = cloudinary.uploader.upload(
+        contents,
+        folder=folder        # e.g "cover_images" or "avatars"
+    )
+
+    # returns the URL
+    return result["secure_url"]
