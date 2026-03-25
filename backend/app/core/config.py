90d6999a634322a82c0e6196bdbc1a498b325125
bonsonalo <bonson2468@gmail.com>
2026-03-26 00:22:41 +0800

    fear: add cloudinary variables

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,8 +5,11 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
+    CLOUD_NAME: str
+    API_KEY: str
+    API_SECRET: str
 
     model_config= SettingsConfigDict(env_file= ".env", env_file_encoding= "utf-8")
 
 
-settings= Settings() 
+settings= Settings()
