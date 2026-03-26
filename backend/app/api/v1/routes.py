c695afbe0894b1d2af7ed6910952565d328db314
bonsonalo <bonson2468@gmail.com>
2026-03-26 23:39:04 +0800

    feat: add notification router

--- backend/app/api/v1/routes.py
+++ backend/app/api/v1/routes.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter
 from app.api.v1.endpoints.auth import router as auth_router
 from app.api.v1.endpoints.article import router as article_router
+from app.api.v1.endpoints.notification import router as notification_router
 
 
 
 
 routers= APIRouter()
 
-router_list= [auth_router, article_router]
+router_list= [auth_router, article_router, notification_router]
 
 for router in router_list:
     routers.include_router(router)
