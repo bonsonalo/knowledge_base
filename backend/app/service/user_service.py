5fd7695af333cbff81ca44588a5cc285b2cee35a
bonsonalo <bonson2468@gmail.com>
2026-03-30 15:43:38 +0800

    fix: remove the schema from the update_profile_service and list each one by one

--- backend/app/service/user_service.py
+++ backend/app/service/user_service.py
@@ -1,7 +1,8 @@
+from fastapi import Form, UploadFile
 from sqlalchemy import select
 from app.model.user import User
 from app.core.logger import logger
-from app.schema.user_schema import ProfileUpdate
+# from app.schema.user_schema import ProfileUpdate
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
@@ -19,18 +20,18 @@ async def user_profile_service(current_user, db: AsyncSession):
 
 #update profile
 
-async def update_profile_service(to_update: ProfileUpdate, current_user, db: AsyncSession):
+async def update_profile_service(current_user, db: AsyncSession, first_name: str | None= Form(None), last_name: str | None= Form(None), avatar: UploadFile | None= Form(None)):
     current_id= current_user["id"]
     user= await db.scalar(select(User).where(User.id == current_id))
 
     if not user:
          raise ValueError("user not available")
-    if to_update.first_name is not None:
-         user.first_name = to_update.first_name
-    if to_update.last_name is not None:
-         user.last_name = to_update.last_name
-    if to_update.avatar is not None:
-         user.avatar = to_update.avatar
+    if first_name is not None:
+         user.first_name = first_name
+    if last_name is not None:
+         user.last_name = last_name
+    if avatar is not None:
+         user.avatar = avatar
 
     db.commit()
     db.refresh(user)
