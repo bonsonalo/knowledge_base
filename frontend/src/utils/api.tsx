d703018c02326dca1c87a6b5efdb2045b1a68470
bonsonalo <bonson2468@gmail.com>
2026-03-30 08:42:56 +0800

    feat: setup api and api interceptor for refresh

---
+++ frontend/src/utils/api.tsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const api= axios.create({
+    baseURL: "http://127.0.0.1:8000",
+    withCredentials: true  // since we're using cookies
+})
+
+export default api
+
+
+
+
+// interceptor for token refresh
+
+api.interceptors.request.use(
+    (response) => response, // if success, pass through
+    async (error) => {
+        if (error.response?.status == 401) { // if 401 error happens(invalid token)
+            try{
+                await api.post("/api/v1/auth/refresh");
+                return api.request(error.config) // Retry original request
+            }
+            catch{
+                // Refresh failed - redirect to login
+                window.location.href= "/login" // we could have used navigate() but window.... is better because it forces it, navigate is with in react, but window is outside of it
+            }
+        }
+        return Promise.reject(error)
+    }
+)
