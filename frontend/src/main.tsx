6fe11802eddbf34af4569c3cdea05c377d7c05bf
bonsonalo <bonson2468@gmail.com>
2026-04-03 17:21:44 +0800

    feat: rwrap the component in BrowserRouter

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import {Provider} from 'react-redux'
 import {store} from './state/store.ts'
-
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </StrictMode>,
 )
