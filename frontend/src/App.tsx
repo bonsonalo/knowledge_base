8e5abc702f5cc875f5af0bce8a2a6d658cdd3464
bonsonalo <bonson2468@gmail.com>
2026-04-05 10:29:28 +0800

    feat: add authInitializer to run when the page loads to get currentUser info if exists

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { LoginPage } from "./pages/LoginPage";
+import { AuthInitializer } from "./components/AuthInitializer";
 
 
 export default function App(){
   return (
     <>
+    <AuthInitializer />
     <Routes>
       <Route path="/login" element= {<LoginPage />}/>
     </Routes>
