4effdeae4713cfaaed78190b4d9cf6c16649bce2
bonsonalo <bonson2468@gmail.com>
2026-04-17 03:21:34 +0800

    feat: add hover effect

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -90,7 +90,7 @@ export function LoginPage() {
                         <input type="checkbox" className="scale-110 cursor-pointer"/>
                         <div className="text-sm" style={{fontWeight: "500"}}>Remember me for 30 days</div>
                     </div>
-                    <div className="flex gap-2 justify-center bg-[#3899FA] mb-5 py-2 rounded-md text-sm"  onClick={handleSubmit}>
+                    <div className="flex gap-2 justify-center bg-[#3899FA] mb-5 py-2 rounded-md text-sm hover:cursor-pointer"  onClick={handleSubmit}>
                         <button className="text-[#fff] hover:cursor-pointer">
                             Sign in
                         </button>
