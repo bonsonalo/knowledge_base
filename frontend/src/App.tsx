49f1fc40546f57723db228e0a65b0ecd84cffc77
bonsonalo <bonson2468@gmail.com>
2026-04-17 18:24:47 +0800

    feat: add NavBar and footer component and add ArticleDertile component. then change the route of the defult to /articles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,17 +3,23 @@ import { LoginPage } from "./pages/LoginPage";
 import { SignUpPage } from "./pages/SignUpPages";
 import { AuthInitializer } from "./components/AuthInitializer";
 import { ArticlePage } from "./pages/ArticlePage";
+import { ArticleDetail } from "./pages/ArticleDetail";
+import { NavBar } from "./components/NavBar";
+import { Footer } from "./components/Footer";
 
 
 export default function App(){
   return (
     <>
     <AuthInitializer />
+    <NavBar />
     <Routes>
       <Route path="/login" element= {<LoginPage />}/>
       <Route path="/signup" element= {<SignUpPage />} />
-      <Route path="/" element= {<ArticlePage />} />
+      <Route index path="/articles" element= {<ArticlePage />} />
+      <Route path= "/:article_id" element= {<ArticleDetail />} />
     </Routes>
+    < Footer />
     </>
   )
 }
