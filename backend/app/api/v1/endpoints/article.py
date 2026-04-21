4a0ffa05f203bbdbc14fa5623c43dafdc8a49247
bonsonalo <bonson2468@gmail.com>
2026-04-21 18:51:09 +0800

    feat: add endpoint for changing the status of article

--- backend/app/api/v1/endpoints/article.py
+++ backend/app/api/v1/endpoints/article.py
@@ -4,7 +4,7 @@ from fastapi import File, Form, HTTPException, APIRouter, UploadFile, status
 
 from app.schema.article_schema import ArticleResponse, Category
 from app.api.deps import editor_dependency, db_dependency
-from app.service.article_service import create_article_draft_service, create_article_publish_service, delete_article_service, delete_article_service_admin, get_all_articles_self_all_service, get_all_articles_self_draft_service, get_all_articles_self_published_service, get_all_articles_service, get_article_editor_service, get_article_service, patch_article_service
+from app.service.article_service import create_article_draft_service, create_article_publish_service, delete_article_service, delete_article_service_admin, get_all_articles_self_all_service, get_all_articles_self_draft_service, get_all_articles_self_published_service, get_all_articles_service, get_article_editor_service, get_article_service, patch_article_service, change_article_status_service
 from app.core.logger import logger
 
 
@@ -59,7 +59,7 @@ async def patch_article(article_id: UUID, current_user:editor_dependency, db: db
     
 
 # get all articles that YOU published or drafted       Editor Role
-@router.get("/self_articles")
+@router.get("/self_articles", response_model=list[ArticleResponse])
 async def get_all_articles_self_all(current_user: editor_dependency, db: db_dependency):
     try:
         result= await get_all_articles_self_all_service(current_user, db)
@@ -121,19 +121,30 @@ async def get_article(article_id: UUID, db: db_dependency):
 
 #get single article     # EDITOR ROLE
 
-@router.get("single_article_editor/{article_id}")
+@router.get("/single_article_editor/{article_id}", response_model=ArticleResponse)
 async def get_article_editor(article_id: UUID, current_user: editor_dependency, db: db_dependency):
     try:
-        await get_article_editor_service(article_id, current_user, db)
+        return await get_article_editor_service(article_id, current_user, db)
     except ValueError as e:
         logger.error(str(e))
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= str(e))
     
 
 
+# change article status (publish / unpublish)   Editor Role
+
+@router.patch("/change_status/{article_id}")
+async def change_article_status(article_id: UUID, new_status: str, current_user: editor_dependency, db: db_dependency):
+    try:
+        return await change_article_status_service(article_id, new_status, current_user, db)
+    except ValueError as e:
+        logger.error(str(e))
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail= str(e))
+
+
 # delete article Editor Role
 
-@router.delete("delete_article_editor/{article_id}")
+@router.delete("/delete_article_editor/{article_id}")
 async def delete_article(article_id: UUID, current_user: editor_dependency, db: db_dependency):
     try:
         await delete_article_service(article_id, current_user, db)
