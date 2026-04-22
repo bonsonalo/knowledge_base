3e173b0488d80d6e3b002ff5bce222127cef485a
bonsonalo <bonson2468@gmail.com>
2026-04-22 13:19:46 +0800

    fx: change id  to user_id in line 27 feat: await db.commit in line 43

--- backend/app/service/notification_service.py
+++ backend/app/service/notification_service.py
@@ -24,7 +24,7 @@ async def create_notification(user_id: UUID, message: str, db: AsyncSession):
 
 async def get_notifications(current_user, db: AsyncSession):
     current_id= current_user["id"]
-    notification= await db.execute(select(Notification).where(Notification.id == current_id).order_by(Notification.created_at.desc()))
+    notification= await db.execute(select(Notification).where(Notification.user_id == current_id).order_by(Notification.created_at.desc()))
 
     return notification.scalars().all()
 
@@ -34,12 +34,12 @@ async def get_notifications(current_user, db: AsyncSession):
 async def mark_as_read(notification_id: UUID, current_user, db: AsyncSession):
     current_id= current_user["id"]
 
-    notification= db.scalar(select(Notification).where(Notification.id == notification_id).where(Notification.user_id == current_id))
+    notification= await db.scalar(select(Notification).where(Notification.id == notification_id).where(Notification.user_id == current_id))
 
     if not notification:
         raise ValueError("Notification not found")
     notification.is_read= True
 
-    db.commit()
-    return {"messgae": "message marked as read"}
+    await db.commit()
+    return {"message": "notification marked as read"}
 
