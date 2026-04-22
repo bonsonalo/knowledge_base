77467a494c5c2fc244c5f460b03f9db18e7fe388
bonsonalo <bonson2468@gmail.com>
2026-04-22 13:21:30 +0800

    feat: add the routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,20 +6,37 @@ import { ArticlePage } from "./pages/ArticlePage";
 import { ArticleDetail } from "./pages/ArticleDetail";
 import { NavBar } from "./components/NavBar";
 import { Footer } from "./components/Footer";
+import { MyArticles } from "./pages/MyArticles";
+import { DashboardLayout } from "./components/DashboardLayout";
+import { Notifications } from "./pages/Notifications";
+import { CreateArticle } from "./pages/CreateArticle";
+import { EditArticle } from "./pages/EditArticle";
+import { Profile } from "./pages/Profile";
 
 
 export default function App(){
   return (
     <>
     <AuthInitializer />
-    <NavBar />
-    <Routes>
-      <Route path="/login" element= {<LoginPage />}/>
-      <Route path="/signup" element= {<SignUpPage />} />
-      <Route index path="/articles" element= {<ArticlePage />} />
-      <Route path= "/:article_id" element= {<ArticleDetail />} />
-    </Routes>
-    < Footer />
+    <div className="min-h-screen flex flex-col">
+      <NavBar />
+      <div className="flex-1 flex flex-col">
+        <Routes>
+          <Route path="/login" element= {<LoginPage />}/>
+          <Route path="/signup" element= {<SignUpPage />} />
+          <Route path="/" element= {<ArticlePage />} />
+          <Route path="/dashboard" element= {< DashboardLayout />}>
+            <Route index element= {<MyArticles />} />
+            <Route path="notifications" element= {<Notifications />} />
+            <Route path="create" element= {<CreateArticle />} />
+            <Route path="edit/:article_id" element= {<EditArticle />} />
+          </Route>
+          <Route path="/profile" element= {<Profile />} />
+          <Route path= "/:article_id" element= {<ArticleDetail />} />
+        </Routes>
+      </div>
+      < Footer />
+    </div>
     </>
   )
 }
