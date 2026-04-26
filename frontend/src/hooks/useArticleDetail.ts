7996d774308a26df54d66adbf152c374808373c8
bonsonalo <bonson2468@gmail.com>
2026-04-26 18:11:37 +0800

    feat; create custom hook for articldetail page

---
+++ frontend/src/hooks/useArticleDetail.ts
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import api from "../utils/api";
+import { useParams } from "react-router-dom";
+import type { Article } from "../types";
+
+
+
+export function useArticleDetail() {
+
+    const [article, setArticle]= useState<Article | null>(null);
+    const [error, setError] = useState("");
+    const { article_id }= useParams();
+
+
+    useEffect(() => {
+    const fetchArticle= async () => {
+        try{
+            const response= await api.get(`/api/v1/article/single_article/${article_id}`);
+            setArticle(response.data);
+        }
+        catch {
+            setError("failed to fetche the article")
+        }
+    }
+    fetchArticle();
+    }, [article_id])
+
+
+
+    return {article, article_id, setError, error}
+    
+}
